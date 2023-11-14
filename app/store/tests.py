796eb60070f2a844660cbb4d15ac33a1270360dc
FIFSAK <anuar200572@gmail.com>
2023-11-14 17:39:06 +0600

    check git ignore

--- app/store/tests.py
+++ app/store/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+try:
+    s3 = boto3.client('s3', aws_access_key_id='AKIAVL2UUMTHDSU4Z4NZ', aws_secret_access_key='YOUR_SECRET_KEY')
+    response = s3.list_buckets()
+    for bucket in response['Buckets']:
+        print(f'Bucket Name: {bucket["Name"]}')
+except NoCredentialsError:
+    print("Credentials not available")
