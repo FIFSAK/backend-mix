d9bb7b4c9f1f282df36250bba5ae51c493f3d410
FIFSAK <anuar200572@gmail.com>
2023-11-18 23:25:42 +0600

    make trousers view

--- app/store/views.py
+++ app/store/views.py
@@ -1,13 +1,18 @@
+import json
+
 from django.core.serializers import serialize
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from .models import *
-
-
+from django.http import JsonResponse, HttpResponse
+from .models import Trousers
 
 def trousers_view(request):
+    """
+    Returns a list of all Trousers objects in JSON format.
+    Only responds to HTTP GET requests.
+    """
     if request.method == "GET":
         objectQuerySet = Trousers.objects.all()
-        data = serialize('json', list(objectQuerySet))
-        return JsonResponse(data, safe=False)
-    return HttpResponse(status=400)
+        data = serialize('json', objectQuerySet)  # Directly pass the queryset
+        data = json.loads(data)
+        return JsonResponse(data, safe=False)  # For an array of JSON objects
+    else:
+        return HttpResponse("Method not allowed", status=405)  # Method Not Allowed status
