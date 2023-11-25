2b82c08569acaf3381884fc8fff28354825b2862
FIFSAK <anuar200572@gmail.com>
2023-11-26 00:06:26 +0600

    add paginaton update requirements txt

--- app/app/settings.py
+++ app/app/settings.py
@@ -152,3 +152,9 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1
+}
