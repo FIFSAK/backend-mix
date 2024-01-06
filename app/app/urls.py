593d5a1dbfc746cb48c04d8e96ef12ad8b0a2671
FIFSAK <anuar200572@gmail.com>
2024-01-06 21:43:07 +0600

    add template and static

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from django.conf import settings
+from django.conf.urls.static import static
 from store.views import *
 
 from rest_framework_simplejwt.views import (
@@ -32,10 +33,12 @@ router.register(r'users', UserView, basename='users')
 router.register(r'cartItem', CartItemViewSet, basename='cartItem')
 
 urlpatterns = [
+    path('', index),
+    path('search.html/', search),
     path('admin/', admin.site.urls),
     # path('clothes/<str:type>', clothes_view),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
--- app/store/views.py
+++ app/store/views.py
@@ -1,45 +1,16 @@
-import json
-from django.core.serializers import serialize
-from django.http import JsonResponse, HttpResponse
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.response import Response
-
+from django.shortcuts import render
 from .models import *
 from rest_framework import viewsets, filters, permissions, status
 from .serializers import ClothesSerializer, UserSerializer, CartItemSerializer
 from django.contrib.auth.models import User
 
+def index(request):
+    return render(request, "index.html")
 
-# def clothes_view(request, type):
-#     """
-#     Returns a list of clothes objects in JSON format based on the type.
-#     Only responds to HTTP GET requests.
-#     """
-#     if request.method != "GET":
-#         return HttpResponse("Method not allowed", status=405)
-#
-#     model_map = {
-#         'Trousers': Trousers,
-#         'TShirtsAndTops': TShirtsAndTops,
-#         'Jacket': Jacket,
-#         'ShirtsAndBlouses': ShirtsAndBlouses,
-#         'Dresses': Dresses,
-#         'OverallsJacketsRaincoatsCardigans': OverallsJacketsRaincoatsCardigans,
-#         'PantsuitsShortsSkirts': PantsuitsShortsSkirts,
-#         'Jeans': Jeans,
-#         'Underwear': Underwear,
-#     }
-#
-#     model = model_map.get(type)
-#     if not model:
-#         return HttpResponse("Invalid type", status=400)
-#
-#     object_query_set = model.objects.all()
-#     data = serialize('json', object_query_set)
-#     data = json.loads(data)
-#     return JsonResponse(data, safe=False)
-
+def search(request):
+    return render(request, "search.html")
 
 class ClothesViewSet(viewsets.ReadOnlyModelViewSet):
     """
